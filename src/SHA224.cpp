b3c1356a16153eecff37f7ef331dfa203a3b1828
Lennart Buhl <git@hackmate.de>
2022-05-17 18:31:46 +0200

    Upgrade to official version 0.4.0

--- src/SHA224.cpp
+++ src/SHA224.cpp
@@ -20,43 +20,51 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef CRYPTO_PROGMEMUTIL_H
-#define CRYPTO_PROGMEMUTIL_H
+#include "SHA224.h"
+#include "Crypto.h"
 
-#if defined(__AVR__)
-#include <avr/pgmspace.h>
-#define pgm_read_qword(x)   \
-    (__extension__ ({ \
-        const uint32_t *_temp = (const uint32_t *)(x); \
-        ((uint64_t)pgm_read_dword(_temp)) | \
-        (((uint64_t)pgm_read_dword(_temp + 1)) << 32); \
-    }))
-#elif defined(ESP8266) || defined(ESP32)
-#include <pgmspace.h>
-#define pgm_read_qword(x)   \
-    (__extension__ ({ \
-        const uint32_t *_temp = (const uint32_t *)(x); \
-        ((uint64_t)pgm_read_dword(_temp)) | \
-        (((uint64_t)pgm_read_dword(_temp + 1)) << 32); \
-    }))
-#else
-#include <string.h>
-#define PROGMEM
-#ifndef pgm_read_byte
-# define pgm_read_byte(x)    (*(x))
-#endif
-#ifndef pgm_read_word
-# define pgm_read_word(x)    (*(x))
-#endif
-#ifndef pgm_read_dword
-# define pgm_read_dword(x)   (*(x))
-#endif
-#ifndef pgm_read_qword
-# define pgm_read_qword(x)   (*(x))
-#endif
-#ifndef memcpy_P
-# define memcpy_P(d,s,l)     memcpy((d), (s), (l))
-#endif
-#endif
+/**
+ * \class SHA224 SHA224.h <SHA224.h>
+ * \brief SHA-224 hash algorithm.
+ *
+ * Reference: http://en.wikipedia.org/wiki/SHA-2
+ *
+ * \sa SHA256, SHA512, SHA3_256, BLAKE2s
+ */
 
-#endif
+/**
+ * \var SHA224::HASH_SIZE
+ * \brief Constant for the size of the hash output of SHA224.
+ */
+
+/**
+ * \var SHA224::BLOCK_SIZE
+ * \brief Constant for the block size of SHA224.
+ */
+
+/**
+ * \brief Constructs a SHA-224 hash object.
+ */
+SHA224::SHA224()
+{
+    reset();
+}
+
+size_t SHA224::hashSize() const
+{
+    return 28;
+}
+
+void SHA224::reset()
+{
+    state.h[0] = 0xc1059ed8;
+    state.h[1] = 0x367cd507;
+    state.h[2] = 0x3070dd17;
+    state.h[3] = 0xf70e5939;
+    state.h[4] = 0xffc00b31;
+    state.h[5] = 0x68581511;
+    state.h[6] = 0x64f98fa7;
+    state.h[7] = 0xbefa4fa4;
+    state.chunkSize = 0;
+    state.length = 0;
+}
